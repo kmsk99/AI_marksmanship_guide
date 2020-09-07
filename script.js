ac6ab1251748b8ab6268e1a455c02510232b30b0
kmsk99 <momoqq@naver.com>
2020-09-07 17:10:33 +0900

    ok

--- script.js
+++ script.js
@@ -106,8 +106,6 @@ function handleError(error) {
 const URL = "./my_model/";
 // 초기 값 설정
 let model,
-    webcam,
-    ctx,
     labelContainer,
     maxPredictions,
     status;
@@ -117,26 +115,16 @@ async function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
-    // load the model and metadata Refer to tmImage.loadFromFiles() in the API to
-    // support files from a file picker
-    // Note: the pose library adds a tmPose object to your window (window.tmPose)
     model = await tmPose.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    // Convenience function to setup a webcam const size = 400; const flip = true;
-    // whether to flip the webcam webcam = new tmPose.Webcam(size, size, flip);
-    // width, height, flip await webcam.setup();  request access to the webcam await
-    // webcam.play(); window.requestAnimationFrame(loop); append/get elements to the
-    // DOM const canvas = document.getElementById("canvas"); canvas.width = size;
-    // canvas.height = size; ctx = canvas.getContext("2d");
+    window.requestAnimationFrame(loop);
+    
     labelContainer = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) { // and class labels
         labelContainer.appendChild(document.createElement("div"));
     }
 }
-
-// async function loop(timestamp) {      webcam.update();  update the webcam
-// frame     await predict();     window.requestAnimationFrame(loop); }
 
 async function predict() {
     // Prediction #1: run input through posenet estimatePose can take in an image,
