883aaaa5d8dedf4cf2a99bca254287644197b45e
kmsk99 <momoqq@naver.com>
2020-09-07 21:22:29 +0900

    ok

--- script.js
+++ script.js
@@ -17,10 +17,8 @@ limitations under the License.
 'use strict';
 
 var videoElement = document.querySelector('video');
-// var audioSelect = document.querySelector('select#audioSource');
 var videoSelect = document.querySelector('select#videoSource');
 
-// audioSelect.onchange = getStream;
 videoSelect.onchange = getStream;
 
 getStream()
@@ -40,9 +38,6 @@ function gotDevices(deviceInfos) {
     for (const deviceInfo of deviceInfos) {
         const option = document.createElement('option');
         option.value = deviceInfo.deviceId;
-        // if (deviceInfo.kind === 'audioinput') {     option.text = deviceInfo.label ||
-        // `Microphone ${audioSelect.length + 1}`;     audioSelect.appendChild(option);
-        // } else
         if (deviceInfo.kind === 'videoinput') {
             option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
             videoSelect.appendChild(option);
@@ -59,11 +54,8 @@ function getStream() {
                 track.stop();
             });
     }
-    // const audioSource = audioSelect.value;
     const videoSource = videoSelect.value;
     const constraints = {
-        // audio: {     deviceId: audioSource         ? {             exact: audioSource
-        // }         : undefined },
         video: {
             deviceId: videoSource
                 ? {
@@ -81,8 +73,6 @@ function getStream() {
 
 function gotStream(stream) {
     window.stream = stream; // make stream available to console
-    // audioSelect.selectedIndex = [...audioSelect.options].findIndex(     option =>
-    // option.text === stream.getAudioTracks()[0].label );
     videoSelect.selectedIndex = [...videoSelect.options].findIndex(
         option => option.text === stream.getVideoTracks()[0].label
     );
