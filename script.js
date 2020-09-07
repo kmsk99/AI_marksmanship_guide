2400b1f3f3895bd8320728074fa9e06704102c09
msk <momoqq@naver.com>
2020-09-07 14:46:00 +0900

    ok

--- script.js
+++ script.js
@@ -102,11 +102,11 @@ async function init() {
 
     // Convenience function to setup a webcam
     const size = 400;
-    const flip = true; // whether to flip the webcam
-    webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
-    window.requestAnimationFrame(loop);
+    // const flip = true; // whether to flip the webcam
+    // webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+    // await webcam.setup(); // request access to the webcam
+    // await webcam.play();
+    // window.requestAnimationFrame(loop);
 
     // append/get elements to the DOM
     const canvas = document.getElementById("canvas");
@@ -120,7 +120,7 @@ async function init() {
 }
 
 async function loop(timestamp) {
-    webcam.update(); // update the webcam frame
+    // webcam.update(); // update the webcam frame
     await predict();
     window.requestAnimationFrame(loop);
 }
@@ -128,7 +128,7 @@ async function loop(timestamp) {
 async function predict() {
     // Prediction #1: run input through posenet estimatePose can take in an image,
     // video or canvas html element
-    const {pose, posenetOutput} = await model.estimatePose(webcam.canvas);
+    const {pose, posenetOutput} = await model.estimatePose(videoElement);
     // Prediction 2: run input through teachable machine classification model
     const prediction = await model.predict(posenetOutput);
 
@@ -141,7 +141,7 @@ async function predict() {
             .innerHTML = classPrediction;
     }
     // finally draw the poses
-    drawPose(pose);
+    // drawPose(pose);
 
     // 음성으로 행동 말해주기
     if (prediction[0].probability.toFixed(2) >= 0.99) {
@@ -175,14 +175,14 @@ async function predict() {
     }
 }
 
-function drawPose(pose) {
-    if (webcam.canvas) {
-        ctx.drawImage(webcam.canvas, 0, 0);
-        // draw the keypoints and skeleton
-        if (pose) {
-            const minPartConfidence = 0.5;
-            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-        }
-    }
-}
+// function drawPose(pose) {
+//     if (webcam.canvas) {
+//         ctx.drawImage(webcam.canvas, 0, 0);
+//         // draw the keypoints and skeleton
+//         if (pose) {
+//             const minPartConfidence = 0.5;
+//             tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+//             tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+//         }
+//     }
+// }
