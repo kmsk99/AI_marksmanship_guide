9e04dcb6b8b4a3a7b8b1819adff481463e1db74c
msk <momoqq@naver.com>
2020-09-07 12:18:14 +0900

    ok

--- script.js
+++ script.js
@@ -2,36 +2,60 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 // the link to your model provided by Teachable Machine export panel 경로
 
-let video = document.getElementById("#videoInput"); // video is the id of video tag
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(function(stream) {
-        video.srcObject = stream;
+(function () {
+
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        video = document.getElementById('video'),
+        vendorUrl = window.URL || window.webkitURL;
+
+    navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetuserMedia || navigator.msGetUserMedia;
+
+    navigator.getMedia({
+        video: true,
+        audio: false
+    }, function (stream) {
+        video.src = vendorUrl.createObjectURL(stream);
         video.play();
-    })
-    .catch(function(err) {
-        console.log("An error occurred! " + err);
+    }, function (error) {
+        // an error occurred
     });
 
-    let canvasFrame = document.getElementById("#canvasFrame"); // canvasFrame is the id of <canvas>
-let context = canvasFrame.getContext("2d");
-let src = new cv.Mat(height, width, cv.CV_8UC4);
-let dst = new cv.Mat(height, width, cv.CV_8UC1);
-const FPS = 30;
-function processVideo() {
-    let begin = Date.now();
-    context.drawImage(video, 0, 0, width, height);
-    src.data.set(context.getImageData(0, 0, width, height).data);
-    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-    cv.imshow("#canvasOutput", dst); // canvasOutput is the id of another <canvas>;
-    // schedule next one.
-    let delay = 1000/FPS - (Date.now() - begin);
-    setTimeout(processVideo, delay);
-}
-// schedule first one.
-setTimeout(processVideo, 0);
+    video.addEventListener('play', function () {
+        draw(this, context, 1024, 768);
+    }, false);
 
+    function draw(video, context, width, height) {
+        var image,
+            data,
+            i,
+            r,
+            g,
+            b,
+            brightness;
 
+        context.drawImage(video, 0, 0, width, height);
 
+        image = context.getImageData(0, 0, width, height);
+        data = image.data;
+
+        for (i = 0; i < data.length; i += 4) {
+            r = data[i];
+            g = data[i + 1];
+            b = data[i + 2];
+            brightness = (r + g + b) / 3;
+
+            data[i] = data[i + 1] = data[i + 2] = brightness;
+        }
+
+        image.data = data;
+
+        context.putImageData(image, 0, 0);
+
+        setTimeout(draw, 10, video, context, width, height);
+    }
+
+})();
 
 const URL = "./my_model/";
 // 초기 값 설정
