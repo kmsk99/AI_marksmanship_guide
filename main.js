7f5095abcc475d683cee08d6d559d8477cea66ba
kmsk99 <momoqq@naver.com>
2020-12-20 21:57:55 +0900

    ok

--- main.js
+++ main.js
@@ -65,8 +65,9 @@ async function init() {
     maxPredictions = model.getTotalClasses();
 
     const flip = true; // whether to flip the webcam
-    webcam = new tmPose.Webcam(200, 200, flip); // width, height, flip
-    await webcam.setup({ facingMode: "environment" }); // request access to the webcam
+    webcam = new tmPose.Webcam(400, 400, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    webcam.setup({ facingMode: "environment" })
     webcam.play();
     // 루프구문
     window.requestAnimationFrame(loop);
