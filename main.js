77ac4582ddd09017d5513fa27b27517529c1e124
kmsk99 <momoqq@naver.com>
2020-12-20 22:08:24 +0900

    ok

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ async function loop(timestamp) {
 async function predict() {
     // Prediction #1: run input through posenet estimatePose can take in an image,
     // video or canvas html element
-    const {pose, posenetOutput} = await model.estimatePose(videoElement, false);
+    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
     // Prediction 2: run input through teachable machine classification model
     const prediction = await model.predict(posenetOutput);
 
