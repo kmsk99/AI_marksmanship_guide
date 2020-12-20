88cea092682264ac57647cd92c94b01d1cfbcde7
kmsk99 <momoqq@naver.com>
2020-12-20 22:01:03 +0900

    ok

--- main.js
+++ main.js
@@ -48,6 +48,9 @@ function poseSelect() {
     });
 };
 
+webcam = new tmPose.Webcam(400, 400, flip); // width, height, flip
+webcam.setup({ facingMode: "environment" })
+
 // 클릭버튼 연결된 함수
 async function init() {
     $(document).ready(function () {
@@ -65,9 +68,7 @@ async function init() {
     maxPredictions = model.getTotalClasses();
 
     const flip = true; // whether to flip the webcam
-    webcam = new tmPose.Webcam(400, 400, flip); // width, height, flip
     await webcam.setup(); // request access to the webcam
-    webcam.setup({ facingMode: "environment" })
     webcam.play();
     // 루프구문
     window.requestAnimationFrame(loop);
